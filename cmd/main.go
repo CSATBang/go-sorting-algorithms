533d36a36dacbc923d1b5fc94fdc4d1289e98b7b
CSATBang <c508428101@outlook.com>
2025-08-29 12:10:54 +0800

    修改main.go

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,17 @@ func cardinalSort(a []int) {
 }
 func main() {
 	arr := []int{3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 47, 4, 19, 50, 48}
-	algorithms.MergeSort(arr)
-	fmt.Println("MergeSort:", arr)
+	bubbleArr := append([]int(nil), arr...)
+	hillArr := append([]int(nil), arr...)
+	insertArr := append([]int(nil), arr...)
+	quickArr := append([]int(nil), arr...)
+	mergeArr := append([]int(nil), arr...)
+	fmt.Println("BubbleSort:", algorithms.BubbleSort(bubbleArr))
+	fmt.Println("HillSort:", algorithms.HillSort(hillArr))
+	fmt.Println("InsertSort:", algorithms.InsertSort(insertArr))
+	fmt.Println("QuickSort:", algorithms.QuickSort(quickArr, 0, len(quickArr)-1))
+	algorithms.MergeSort(mergeArr)
+	fmt.Println("MergeSort:", mergeArr)
+	fmt.Println("arr:", arr)
+
 }
